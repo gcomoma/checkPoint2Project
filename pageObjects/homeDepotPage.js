b505069039ecc28abb1dae6a75a78ff242ec2be3
Gregory Omoma <omomagreg@yahoo.com>
2020-05-28 17:15:03 -0400

    k

--- pageObjects/homeDepotPage.js
+++ pageObjects/homeDepotPage.js
@@ -15,5 +15,23 @@ module.exports = {
             {selector: '//span[text()="Skip for now & Continue Shopping"]', locateStrategy: 'xpath'},
         register:
             {selector: '//span[text()="Register"]', locateStrategy: 'xpath'},
+        homeDecor:
+            {selector: '//a[text()="Home Decor & Furniture"]', locateStrategy: 'xpath'},
+        furniture:
+            {selector: '//a[text()="Furniture"]', locateStrategy: 'xpath'},
+        wallDecor:
+            {selector: '//a[text()="Wall Decor"]', locateStrategy: 'xpath'},
+        kitchenDining:
+            {selector: '//a[text()="Kitchen & Dining"]', locateStrategy: 'xpath'},
+        bedBath:
+            {selector: '//a[text()="Bedding & Bath"]', locateStrategy: 'xpath'},
+        lighting:
+            {selector: '//a[text()="Lighting"]', locateStrategy: 'xpath'},
+        window:
+            {selector: '//a[text()="Window Treatments"]', locateStrategy: 'xpath'},
+        header:
+            {selector: '//h1', locateStrategy: 'xpath'},
+        home:
+            {selector: '//a[text()="Home"]', locateStrategy: 'xpath'},
     },  
 }
--- tests/homeDepot.js
+++ tests/homeDepot.js
@@ -1,3 +1,5 @@
+var homeDepotArray2 = require('../testsAssets/homeDepotArray2')
+var homeDepotArray = require('../testsAssets/homeDepotArray')
 var homeDepot = {}
 module.exports = {
     before: browser => {
@@ -8,26 +10,52 @@ module.exports = {
     after: browser => {
         homeDepot.end()
     },
-    'sign up test': browser => {
-        homeDepot
-            .click('@myAccount')
-            .click('@register')
-            .setValue('@enterEmail', 'omomagreg@gamil.com')
-            .setValue('@enterPw', 'Pokemon2')
-            .setValue('@enterZip', '11413')
-            .setValue('@enterPhone', '6466517770')
-            .click('@createAcct')
-            .click('@skipForNow')
-    },
-    'sign in test': browser => {
-        homeDepot
-            .click('@myAccount')
-            .click('@signInBtn')
-            .setValue('@enterEmail', 'omomagreg@yahoo.com')
-            .setValue('@enterPw', 'Pokemon1')
-            .click('@signInBtn')
-    },
+    // 'sign up test': browser => {
+    //     homeDepot
+    //         .click('@myAccount')
+    //         .click('@register')
+    //         .setValue('@enterEmail', 'omomagreg@gamil.com')
+    //         .setValue('@enterPw', 'Pokemon2')
+    //         .setValue('@enterZip', '11413')
+    //         .setValue('@enterPhone', '6466517770')
+    //         .click('@createAcct')
+    //         .click('@skipForNow')
+    // },
+    // 'sign in test': browser => {
+    //     homeDepot
+    //         .click('@myAccount')
+    //         .click('@signInBtn')
+    //         .setValue('@enterEmail', 'omomagreg@yahoo.com')
+    //         .setValue('@enterPw', 'Pokemon1')
+    //         .click('@signInBtn')
+    // },
     'verify featured categories': browser => {
-        
+        var homeDepotObject = {
+            process: (pageObject, item) => {
+                pageObject
+                    .click('@homeDecor')
+                    .click(item.category)
+                    .verify.containsText('@header', item.result)
+                    .click('@home')
+            }
+        }
+        homeDepotArray.forEach(info => {
+            homeDepotObject.process(homeDepotPage, info)
+        })
+
+    },
+    'verify featured categories 2': broswer => {
+        var homeDepotObject2 = {
+            stage: (pageObject, piece) => {
+                pageObject
+                    .useXpath()
+                    .click(`(//img[@class="visualNav__img"])[${piece.number}]`)
+                    .verify.containsText(piece.result)
+                    .click('@home')
+            }
+        }
+        homeDepotArray2.forEach(data => {
+            homeDepotObject2.stage(homeDepotPage, data)
+        })
     }
 }   
